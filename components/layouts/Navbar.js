7aac438b8faefdb58f0520008ee9a62cf969862b
MengAo64 <samueluno64@gmail.com>
2023-05-20 22:09:29 +0700

    update auth

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -25,10 +25,12 @@ import MenuIcon from "@mui/icons-material/Menu";
 import AdbIcon from "@mui/icons-material/Adb";
 import { BookmarkBorder } from "@mui/icons-material";
 import Image from "next/image";
+import pb from "@/lib/pocketbase";
 // import logo from '../public/brand/logo.png';
+import useLogout from "@/hooks/useLogout";
 
 const pages = ["Saved"];
-const settings = ["Profile", "Logout"];
+// const settings = ["Profile", "Logout"];
 
 const bull = (
   <Box
@@ -43,7 +45,7 @@ export default function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const router = useRouter();
-
+  const logout = useLogout();
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
@@ -60,10 +62,14 @@ export default function Navbar() {
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: "white", color: "black" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+    <>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "white", color: "black" }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
             <Typography
             variant="h6"
             noWrap
@@ -82,123 +88,138 @@ export default function Navbar() {
             LOGO
             </Typography> */}
 
-          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
+            <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <Image
+                src="/brand/title.png"
+                alt="the logo"
+                width={230}
+                height={230}
+              />
+            </Box>
+
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <Image
+                src="/brand/logoonly.png"
+                alt="the logo"
+                width={50}
+                height={50}
+              />
+            </Box>
+
+            <Box
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              className="flex justify-end"
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "inherit", display: "block" }}
+                >
+                  <BookmarkBorder />
+                  {page}
+                </Button>
               ))}
-            </Menu>
-          </Box>
+            </Box>
 
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Image
-              src="/brand/title.png"
-              alt="the logo"
-              width={230}
-              height={230}
-            />
-          </Box>
-
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <Image
-              src="/brand/logoonly.png"
-              alt="the logo"
-              width={50}
-              height={50}
-            />
-          </Box>
-
-          <Box
-            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-            className="flex justify-end"
-          >
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "inherit", display: "block" }}
-              >
-                <BookmarkBorder />
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          {/* UNAUTHENTICATED */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Button
-              className="mx-1"
-              color="inherit"
-              onClick={() => router.push("/sign-in")}
-            >
-              Login
-            </Button>
-            <Button variant="contained" className="bg-blue-500 mx-1">
-              Register
-            </Button>
-          </Box>
-
-          {/* AUTHENTICATED */}
-          {/* <Box sx={{ flexGrow: 0 }}>
+            {/* UNAUTHENTICATED */}
+            {pb.authStore.isValid ? (
+              <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
                 </Tooltip>
                 <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
                 >
-                    {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                    </MenuItem>
-                    ))}
+                  {/* {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))} */}
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Profile</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Typography textAlign="center" onClick={logout}>
+                      Logout
+                    </Typography>
+                  </MenuItem>
                 </Menu>
-            </Box> */}
-        </Toolbar>
-      </Container>
-    </AppBar>
+              </Box>
+            ) : (
+              <Box sx={{ flexGrow: 0 }}>
+                <Button
+                  className="mx-1"
+                  color="inherit"
+                  onClick={() => router.push("/sign-in")}
+                >
+                  Login
+                </Button>
+                <Button variant="contained" className="bg-blue-500 mx-1">
+                  Register
+                </Button>
+              </Box>
+            )}
+
+            {/* AUTHENTICATED */}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
