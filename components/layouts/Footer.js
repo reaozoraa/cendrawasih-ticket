88f82820200dc85ca6be68311f8e8f8ec7daba44
jagadsatria11 <jagads535@gmail.com>
2023-05-24 10:37:24 +0700

    membuat footer

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -3,8 +3,12 @@ import {
     Container, Avatar, Button, Tooltip, MenuItem, Card, CardActions,
     CardContent, Tabs, Tab, Paper } 
 from '@mui/material/'
-
+import Link from 'next/link';
 import { styled } from '@mui/material/styles';
+
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 
 export default function Footer() {
@@ -35,42 +39,32 @@ export default function Footer() {
             display: "flex",
             mb: 2,
           }}
-          className=' flex flex-wrap justify-evenly'
+          className=' flex flex-wrap justify-evenly text-white'
         >
           <Box>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
+            <Typography variant="h5">Ikuti Kami</Typography>
+            <h3><Link href="https://www.instagram.com/cendrawasihtravel10/"><InstagramIcon sx={{ fontSize: 30 }} /></Link><Link href="https://www.facebook.com/profile.php?id=100092747504784"><FacebookIcon sx={{ fontSize: 30 }} /></Link><Link href="https://twitter.com/Cendrawasih1010"><TwitterIcon sx={{ fontSize: 30 }} /></Link></h3>
           </Box>
           
           <Box>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
-            <h3>bbbbbbbbbbbbbbbbbbbbbbbbbbbbb</h3>
+            <Typography variant="h5">Layanan</Typography>
+            <h3>Bantuan</h3>
+            <h3>Travel</h3>
+            <h3>Hotel</h3>
           </Box>
           
           <Box>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
-            <h3>cccccccccccccccccccccccccccccccc</h3>
+            <Typography variant="h5">Jelajahi</Typography>
+            <h3></h3>
+            <h3>Tentang Kami</h3>
+            <h3>Kontak Media</h3>
+            <h3>Blog</h3>
           </Box>
           
           <Box>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
-            <h3>ddddddddddddddddddddddddddddddddd</h3>
+            <Typography variant="h5">Toko Kami</Typography>
+            <h3><Link href="https://www.google.com">Tiket Pesawat</Link></h3>
+            <h3>Tiket Hotel</h3>
           </Box>
         </Box>
         <Box
